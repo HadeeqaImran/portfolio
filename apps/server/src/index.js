b7f927cf8916bf2f12ad9895e5858caafd787484
Hadeeqa Imran <hadeeqa.imran@dubizzlelabs.com>
2025-04-17 12:53:10 +0500

    Remove .env files from tracking

--- apps/server/src/index.js
+++ apps/server/src/index.js
@@ -1,7 +1,39 @@
-import app from './server.js';
+// index.js
+const express = require('express');
+const app = express();
+const { swaggerUi, swaggerSpec } = require('./config/swagger'); // Import Swagger UI and spec
+const userRoutes = require('./routes/user');
+const authRoutes = require('./routes/auth');
+const orderRoutes = require('./routes/order');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
 
-const PORT = process.env.PORT || 5000;
+const dotenv = require('dotenv');
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const env = process.env.NODE_ENV || 'development';
+dotenv.config({ path: `.env.${env}` });
+
+require('./config/passport');
+
+app.use(passport.initialize());
+app.use(express.json());
+app.use(cookieParser());
+
+// Serve the Swagger API docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Auth routes
+app.use('/api/auth', authRoutes);
+
+// User routes
+app.use('/api/users', userRoutes);
+
+// Order Routes
+app.use('/api/orders', orderRoutes);
+
+// Simple route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
+
+module.exports = app;
