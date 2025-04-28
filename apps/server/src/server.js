0a0bda3622498a86cebda4571ca558fc271dda04
Hadeeqa Imran <hadeeqa.imran@dubizzlelabs.com>
2025-04-28 15:20:54 +0500

    wrong library import corrected

--- apps/server/src/server.js
+++ apps/server/src/server.js
@@ -1,6 +1,6 @@
 const connectToDatabase = require('./config/database');
 const app = require('./index');
-
+const dotenv = require('dotenv');
 (async () => {
 
     const envFile = process.env.NODE_ENV === 'production' ? '.env.staging' : '.env.development';
